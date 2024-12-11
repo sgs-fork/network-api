3a8a0488ba739ef394adda88a3e85fad84864433
SigeShuo <sigeshuo@outlook.com>
2024-12-11 20:45:14 +0800

    fix

--- docker-build.sh
+++ docker-build.sh
@@ -1,9 +1,17 @@
 #!/usr/bin/env bash
 
+BUILDER_NAME=default_builder
+if ! docker buildx ls | grep -q "${BUILDER_NAME}"; then
+    docker buildx create --name "${BUILDER_NAME}" --use
+    docker buildx inspect "${BUILDER_NAME}" --bootstrap
+else
+    docker buildx use "${BUILDER_NAME}"
+fi
+
 NEXUS_VERSION=1.0
 
 docker buildx create --use
-docker buildx build --platform linux/amd64,linux/arm64 --load -t sigeshuo/nexus:${NEXUS_VERSION} -t sigeshuo/nexus:latest . --no-cache
+docker buildx build --platform linux/amd64,linux/arm64 -t sigeshuo/nexus:1.0 -t sigeshuo/nexus:latest . --no-cache --push
 
 #docker login
 #
